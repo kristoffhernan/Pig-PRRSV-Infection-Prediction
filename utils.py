81b245b8d5e979891aeee949087a5ba2f1a8f1cf
Kristoffer Hernandez <khern045@scf-sm13.Berkeley.EDU>
2023-05-06 16:40:49 -0700

    knn imputation method

--- utils.py
+++ utils.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d
 from scipy.interpolate import splev, splrep
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import GridSearchCV
 
 
-def na_split(gene, base_df):
-    eg = base_df[[gene, 'dpi']]
+# Splits data into those with and without na given a gene
+def na_split(gene, gd_df):
+    eg = gd_df[[gene, 'dpi']]
     eg = eg.reset_index()
 
     # df with x as days and y and individual
@@ -21,14 +24,14 @@ def na_split(gene, base_df):
     return(w_na, wo_na)
 
 
-
+# includes two different imputing methods to estimate the dpi or gene expression one of the two
 def impute_methods(w_na, wo_na):
 
     x = np.array(wo_na['day'])
     y = np.array(wo_na['ge'])
     x_test = np.array(w_na['day'])
 
-    # interpolation regression
+    # Linear interpolation regression
     interp_func = interp1d(x, y, kind='linear', bounds_error=False, assume_sorted=False)
 
     # evaluate the interpolation function at new x-values
@@ -58,4 +61,50 @@ def impute_methods(w_na, wo_na):
     poly_df = pd.concat([pd.DataFrame(poly_vals), wo_na], axis=0).sort_values('day')
     
     
-    return(poly_df, poly_y, interp_df, interp_y)
+    # KNN regression
+    # kneighbors only accepts 2d arrays, so may into array
+    # then reshape it, -1 is 1 shape dimension, then 1 is 1 column
+    x = np.array(wo_na['day']).reshape(-1, 1)
+    y = np.array(wo_na['ge']).reshape(-1, 1)
+
+    x_test = np.array(w_na['day']).reshape(-1, 1)
+
+    
+    # Nearest Neighbors regression
+    # hyperparameters to search over
+    param_grid = {'n_neighbors': [1, 3, 5, 7, 9, 10],
+                 'p': [1,2]}
+
+    # KNN regressor 
+    knn = KNeighborsRegressor(n_jobs=2)
+
+    # create a grid search object to search over the hyperparameters
+    # by default, gridsearchcv tries to maximize whatever is inside of the scoring parameter
+    # so by putting in the -mse, it will maximize it, essentially minimising the mse which is what we wants
+    grid_search = GridSearchCV(knn, param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=2)
+
+    # fit the grid search
+    grid_search.fit(x, y)
+
+    # append the best score
+    knn_mse = -grid_search.best_score_
+
+    # get the best hyperparameters from the grid search
+    best_params = grid_search.best_params_
+
+    # create a new KNN regressor with the best hyperparameters
+    knn_func = KNeighborsRegressor(n_neighbors=best_params['n_neighbors'], 
+                                   p=best_params['p'], n_jobs=2)
+    knn_func.fit(x, y)
+
+    # impute based on x test
+    knn_y = knn_func.predict(x_test)
+   
+    # impute values to df, use ravel to unlist to be 1d array
+    knn_vals = {'day': x_test.ravel(), 'ge':knn_y.ravel()}
+
+    # combine imputed df with df wo na's
+    knn_df = pd.concat([pd.DataFrame(knn_vals), wo_na], axis=0).sort_values('day') 
+    
+    
+    return(poly_df, poly_func, interp_df, interp_func, knn_df, knn_func, knn_mse)
